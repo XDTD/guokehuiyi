f1b7d956ade2e9d075b02af54285a80eeb1e490f
XDTD <1355582364@qq.com>
2021-11-28 17:25:34 +0800

    完成和livego配合，老师直播信息提示和学生直播链接跳转

---
+++ canvas/node_modules/@instructure/ui-forms/node_modules/@instructure/ui-text-area/lib/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "TextArea", {
+  enumerable: true,
+  get: function get() {
+    return _TextArea.TextArea;
+  }
+});
+
+var _TextArea = require("./TextArea");
---
+++ canvas/node_modules/@instructure/ui-forms/node_modules/@instructure/ui-text-input/lib/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "TextInput", {
+  enumerable: true,
+  get: function get() {
+    return _TextInput.TextInput;
+  }
+});
+
+var _TextInput = require("./TextInput");
